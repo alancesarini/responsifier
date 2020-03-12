6b31d9de4da204675b85a4079164b5d0fd135cd3
alancesarini <alan@alancesarini.com>
2020-03-12 19:57:59 +0100

    Added support for tables

--- src/responsifier.js
+++ src/responsifier.js
@@ -14,8 +14,8 @@ const responsify = () => {
 
 const applyStyles = DOMElement => {
   const tagName = DOMElement.tagName
-  const blockTags = ['BODY', 'HEADER', 'FOOTER', 'DIV', 'UL', 'OL', 'TABLE']
-  const textTags = ['P', 'LI', 'SPAN', 'EM', 'STRONG', 'INPUT', 'BUTTON', 'TH', 'TD']
+  const blockTags = ['BODY', 'HEADER', 'FOOTER', 'DIV', 'UL', 'OL']
+  const textTags = ['P', 'LI', 'SPAN', 'EM', 'STRONG', 'INPUT', 'BUTTON']
   const titleTags = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6']
 
   if (blockTags.find(item => item === tagName)) {
@@ -56,9 +56,9 @@ const applyStyles = DOMElement => {
     DOMElement.style.fontSize = '1.4rem'
   }
 
-  if (tagName === 'TH' || tagName === 'TD') {
-    DOMElement.style.width = '100%'
-    DOMElement.style.height = 'auto'
+  if (tagName === 'TABLE') {
+    DOMElement.parentNode.insertBefore(createResponsiveTable(DOMElement), DOMElement)
+    DOMElement.remove()
   }
 
   if (DOMElement.hasChildNodes()) {
@@ -73,6 +73,69 @@ const applyStyles = DOMElement => {
     applyStyles(DOMElement)
     return false
   }
+}
+
+const createResponsiveTable = DOMElement => {
+  const firstRow = DOMElement.querySelector('TR')
+  const responsiveTable = document.createElement('DIV')
+  const arrayHeader = []
+
+  responsiveTable.style.width = '100%'
+
+  responsiveTable.style.float = 'left'
+  responsiveTable.style.margin = '1rem 0'
+
+  if (firstRow === null) {
+    return false
+  }
+
+  let headerColumns = firstRow.querySelectorAll('TH')
+
+  if (headerColumns === null) {
+    headerColumns = firstRow.querySelectorAll('TD')
+  }
+
+  headerColumns.forEach(item => {
+    const headerColumn = document.createElement('DIV')
+    headerColumn.textContent = item.textContent
+    headerColumn.style.width = '50%'
+    headerColumn.style.float = 'left'
+    headerColumn.style.fontWeight = 'bold'
+    arrayHeader.push(headerColumn)
+  })
+
+  const allRows = DOMElement.querySelectorAll('TR')
+
+  allRows.forEach(row => {
+    const columns = row.querySelectorAll('TD')
+    for (let j = 0; j < columns.length; j++) {
+      const column = document.createElement('DIV')
+      column.textContent = columns[j].textContent
+      column.style.width = '50%'
+      column.style.float = 'left'
+      column.style.paddingLeft = '1rem'
+      column.style.boxSizing = 'border-box'
+
+      const row = document.createElement('DIV')
+      row.style.width = '100%'
+      row.style.float = 'left'
+      row.style.marginBottom = '1rem'
+      row.style.paddingBottom = '1rem'
+      row.style.borderBottom = '1px solid #c0c0c0'
+
+      const clonedHeader = arrayHeader[j].cloneNode(true)
+      row.appendChild(clonedHeader)
+      row.appendChild(column)
+
+      // console.log(row)
+
+      responsiveTable.appendChild(row)
+    }
+  })
+
+  // console.log(responsiveTable)
+
+  return responsiveTable
 }
 
 document.addEventListener('DOMContentLoaded', responsify)
